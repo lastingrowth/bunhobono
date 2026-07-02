ab7bf38777f45d030d40bc80cf82515ddd576ef8
lastingrowth <lastingrowth@gmail.com>
2026-07-02 13:19:44 +0900

    spring + vue connect

---
+++ bunhobono/front/src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import TestView from '@/views/TestView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: HomeView,
+    // },
+    {
+      path: '/test',
+      name: 'test',
+      component: TestView,
+    },
+  ],
+})
+
+export default router
