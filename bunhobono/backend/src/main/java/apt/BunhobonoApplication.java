690826585c128eecaeb68ddf2897928a99bc5bef
DESKTOP-VQFPV17\503-22 <fvvc3000@gmail.com>
2026-07-03 18:32:56 +0900

    커미ㅅ

---
+++ bunhobono/backend/src/main/java/apt/BunhobonoApplication.java
@@ -0,0 +1,13 @@
+package apt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BunhobonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BunhobonoApplication.class, args);
+	}
+
+}
