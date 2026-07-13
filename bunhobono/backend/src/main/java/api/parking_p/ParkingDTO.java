62a50e329b19e2b2a996791816695707b319c2b8
DESKTOP-VQFPV17\503-22 <fvvc3000@gmail.com>
2026-07-13 10:30:45 +0900

    no message

--- bunhobono/backend/src/main/java/api/parking_p/ParkingDTO.java
+++
@@ -1,9 +0,0 @@
-package api.parking_p;
-
-import lombok.Data;
-
-@Data
-public class ParkingDTO {
-    private int parkingNo, displayNo, availableSpaces, parkingSpaces;
-    private String parkingName,  parkingLocation;
-}
--- bunhobono/frontend/src/features/camera/cameraApi.js
+++
@@ -1,6 +0,0 @@
-import api from "@/shared/api/apiClient";
-
-// 회원 전체 조회
-export const getCameraList = () => {
-    return api.get("/cameras");
-};
