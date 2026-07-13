5556e1b6c067c43e93624e0e8771477cb1674eea
DESKTOP-VQFPV17\503-22 <fvvc3000@gmail.com>
2026-07-13 10:30:45 +0900

    no message

--- bunhobono/backend/src/main/java/api/gate_p/GateDTO.java
+++
@@ -1,9 +0,0 @@
-package api.gate_p;
-
-import lombok.Data;
-
-@Data
-public class GateDTO {
-    private int gateNo, parkingNo;
-    private String gateName, gateType, parkingName, parkingLocation;
-}
--- bunhobono/frontend/src/features/carlog/carlogApi.js
+++
@@ -1,8 +0,0 @@
-import api from "@/shared/api/apiClient"
-
-// 차량 입출차 목록 조회
-export const getCarLogs = (params) => {
-    return api.get("/carlog/list", {
-        params,
-    });
-};
