242d638a25037b769a6c0851675238663bdf6762
DESKTOP-VQFPV17\503-22 <fvvc3000@gmail.com>
2026-07-13 13:02:14 +0900

    07-13 카메라목록 백엔드, 프론트 수정

--- bunhobono/frontend/src/features/camera/CameraList.vue
+++ bunhobono/frontend/src/features/camera/CameraList.vue
@@ -2,20 +2,27 @@
   <div>
     <h2>카메라 목록</h2>
     <table border="1">
-      <tr>
-        <td>카메라 번호</td>
-        <td>출입구 번호</td>
-        <td>카메라 이름</td>
-        <td>카메라 종류</td>
-        <td>설치 날짜</td>
-      </tr>
-      <tr v-for="c in cStore.list" :key="c.cameraNo">
-        <td>{{ c.cameraNo }}</td>
-        <td>{{ c.gateNo }}</td>
-        <td>{{ c.cameraName }}</td>
-        <td>{{ c.cameraType }}</td>
-        <td>{{ c.installDate }}</td>
-      </tr>
+      <thead>
+        <tr>
+          <th>카메라 번호</th>
+          <th>게이트 이름</th>
+          <th>주차장 이름</th>
+          <th>카메라 이름</th>
+          <th>카메라 종류</th>
+          <th>설치 날짜</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="c in cStore.list" :key="c.cameraNo">
+          <td>{{ c.cameraNo }}</td>
+          <td>{{ c.gateName ?? '-' }}</td>
+          <td>{{ c.parkingName ?? '-' }}</td>
+          <td>{{ c.cameraName }}</td>
+          <td>{{ c.cameraType }}</td>
+          <td>{{ c.installDate }}</td>
+        </tr>
+      </tbody>
     </table>
   </div>
 </template>
